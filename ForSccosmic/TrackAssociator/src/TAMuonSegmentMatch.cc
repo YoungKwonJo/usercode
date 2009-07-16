1eefe2f6d1efea5a7722128d6262c65209020c1d
youngjo <>
2009-07-16 18:53:45 +0000

    *** empty log message ***

---
+++ ForSccosmic/TrackAssociator/src/TAMuonSegmentMatch.cc
@@ -0,0 +1,4 @@
+#include "ForSccosmic/TrackAssociator/interface/TAMuonSegmentMatch.h"
+#include "DataFormats/MuonDetId/interface/DTChamberId.h"
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
