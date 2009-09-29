1a6f7cf0b74ba22008ea1611e39091feaa05c6aa
youngjo <>
2009-09-29 11:57:39 +0000

    file for submit at batch

---
+++ copyFromCastorToWeb/copyjob.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /afs/cern.ch/cms/sw/cmsset_default.sh
+project CMSSW
+cmsrel CMSSW CMSSW_3_1_1
+cd CMSSW_3_1_1/src/
+eval `scramv1 runtime -sh`
+cvs co -r V00-00-01 UserCode/youngjo/copyFromCastorToWeb
+cd UserCode/youngjo/copyFromCastorToWeb
+
+./checkgif.sh
+
+
+# example
+# > bsub -q 1nd copyjob.sh
