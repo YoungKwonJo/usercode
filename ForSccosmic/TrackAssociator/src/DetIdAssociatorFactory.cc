749c7b95a135068344baf0d2755719aa9c41af7a
youngjo <>
2010-08-04 15:21:27 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-00-09'.
    Sprout from master 2010-08-04 15:21:26 UTC youngjo 'for collision'
    Delete:
        ForSccosmic/TrackAssociator/BuildFile
        ForSccosmic/TrackAssociator/interface/CachedTrajectory.h
        ForSccosmic/TrackAssociator/interface/CaloDetIdAssociator.h
        ForSccosmic/TrackAssociator/interface/DetIdAssociator.h
        ForSccosmic/TrackAssociator/interface/DetIdAssociatorFactory.h
        ForSccosmic/TrackAssociator/interface/DetIdInfo.h
        ForSccosmic/TrackAssociator/interface/EcalDetIdAssociator.h
        ForSccosmic/TrackAssociator/interface/FiducialVolume.h
        ForSccosmic/TrackAssociator/interface/HODetIdAssociator.h
        ForSccosmic/TrackAssociator/interface/HcalDetIdAssociator.h
        ForSccosmic/TrackAssociator/interface/MuonDetIdAssociator.h
        ForSccosmic/TrackAssociator/interface/MuonSegmentMatchCollection.h
        ForSccosmic/TrackAssociator/interface/TAMuonChamberMatch.h
        ForSccosmic/TrackAssociator/interface/TAMuonSegmentMatch.h
        ForSccosmic/TrackAssociator/interface/TrackAssociatorParameters.h
        ForSccosmic/TrackAssociator/interface/TrackDetMatchInfo.h
        ForSccosmic/TrackAssociator/interface/TrackDetMatchInfoCollection.h
        ForSccosmic/TrackAssociator/interface/TrackDetectorAssociator.h
        ForSccosmic/TrackAssociator/src/CachedTrajectory.cc
        ForSccosmic/TrackAssociator/src/DetIdAssociator.cc
        ForSccosmic/TrackAssociator/src/DetIdAssociatorFactory.cc
        ForSccosmic/TrackAssociator/src/DetIdInfo.cc
        ForSccosmic/TrackAssociator/src/FiducialVolume.cc
        ForSccosmic/TrackAssociator/src/MuonDetIdAssociator.cc
        ForSccosmic/TrackAssociator/src/TAMuonChamberMatch.cc
        ForSccosmic/TrackAssociator/src/TAMuonSegmentMatch.cc
        ForSccosmic/TrackAssociator/src/TrackAssociatorParameters.cc
        ForSccosmic/TrackAssociator/src/TrackDetMatchInfo.cc
        ForSccosmic/TrackAssociator/src/TrackDetectorAssociator.cc
        ForSccosmic/TrackAssociator/src/modules.cc
        MyEff/BuildFile
        MyEff/src/MyHistoClassDbeNew.h
        MyEff/src/RPCEffTrackExtrapolationNew.h
        MyEff/src/RPCEffTrackExtrapolationNewFinal_dbe.cc
        MyEff/test/TrackEff.py
        MyEff/test/TrackEff1.py
        RecoLocalMuon/TracktoRPCEff/BuildFile
        RecoLocalMuon/TracktoRPCEff/interface/TracktoRPCEff.h
        RecoLocalMuon/TracktoRPCEff/src/TracktoRPCEff.cc
        RecoLocalMuon/TracktoRPCEff/test/tracktorpceff_cfg.py
        copyFromCastorToWeb/checkgif.sh
        copyFromCastorToWeb/copyjob.sh
        copyFromCastorToWeb/getrun.sh
        copyFromCastorToWeb/mkdirAtServer.py
        copyFromCastorToWeb/submitToWeb.py
        sccosmic/CosmicAnalyzer/BuildFile
        sccosmic/CosmicAnalyzer/src/CosmicAnalyzer.cc
        sccosmic/CosmicAnalyzer/test/BuildFile
        sccosmic/CosmicAnalyzer/test/TrajectoryRPCEff.cc
        sccosmic/CosmicAnalyzer/test/cosmicanalyzer_cfg.py
        sccosmic/CosmicAnalyzer/test/trajectoryRPCeffForMC_cfg.py
        sccosmic/CosmicAnalyzer/test/trajectoryRPCeff_cfg.py

--- ForSccosmic/TrackAssociator/src/DetIdAssociatorFactory.cc
+++
@@ -1,5 +0,0 @@
-#include "FWCore/PluginManager/interface/PluginFactory.h"
-#include "ForSccosmic/TrackAssociator/interface/DetIdAssociatorFactory.h"
-
-EDM_REGISTER_PLUGINFACTORY(DetIdAssociatorFactory, "DetIdAssociatorFactory");
-
